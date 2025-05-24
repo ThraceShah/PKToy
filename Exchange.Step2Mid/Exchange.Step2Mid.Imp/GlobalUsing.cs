a0ee928fe06669d3f68cc6244a2818501bfb65c8
thrace <xqy1739265310@163.com>
2025-05-24 11:39:53 +0800

    Refactor code structure and remove redundant sections for improved readability and maintainability

---
+++ Exchange.Step2Mid/Exchange.Step2Mid.Imp/GlobalUsing.cs
@@ -0,0 +1,11 @@
+global using Exchange.Midlayer;
+global using StepCodeDotNet.Base;
+#if AP_VERSION_203
+global using StepCodeDotNet.Gen.config_control_design;
+#endif
+#if AP_VERSION_203E2
+global using StepCodeDotNet.Gen.ap203_configuration_controlled_3d_design_of_mechanical_parts_and_assemblies_mim_lf;
+#endif
+#if AP_VERSION_214E3
+global using StepCodeDotNet.Gen.automotive_design;
+#endif
